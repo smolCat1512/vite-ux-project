c2552e1fcba60f62f3bb981d88c20782cff2aee6
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-09-28 15:16:05 +0100

    chore: Installed Mantine, starting using it
    Installed and resolved issues.
    Need to rework components, as light mode site now, may need a theming piece creating to help.

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.tsx";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <ScrollToTop />
-      <App />
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <App />
+      </BrowserRouter>
+    </MantineProvider>
   </StrictMode>
 );
