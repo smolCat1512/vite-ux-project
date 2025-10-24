9b0eca03c9606f8c73a02422444a4a42e14ceed1
ShaunH1512 <shaunhalliday1512@gmail.com>
2025-10-24 14:44:01 +0100

    chore: Add new technologies and improve grid responsiveness
    Added several new technologies (HTML5, CSS3, Sass, Cloudflare, Netlify, Framer, Sketch, Vite, NPM, Storybook, Mantine, Shadcn) to the list. Updated the grid layout for better responsiveness across screen sizes and adjusted card width for improved appearance.

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -5,6 +5,18 @@ import { RiNextjsFill } from "react-icons/ri";
 import { BiLogoTypescript } from "react-icons/bi";
 import { FaFigma } from "react-icons/fa";
 import { RiJavascriptFill } from "react-icons/ri";
+import { SiFramer } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { TbBrandVite } from "react-icons/tb";
+import { FaNpm } from "react-icons/fa";
+import { FaSketch } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { SiMantine } from "react-icons/si";
+import { SiShadcnui } from "react-icons/si";
+import { FaCloudflare } from "react-icons/fa";
+import { BiLogoNetlify } from "react-icons/bi";
+import { TbBrandStorybook } from "react-icons/tb";
 
 const technologies = [
   {
@@ -28,8 +40,28 @@ const technologies = [
     icon: RiJavascriptFill,
   },
   {
+    name: "HTML5",
+    icon: FaHtml5,
+  },
+  {
+    name: "CSS3",
+    icon: FaCss3Alt,
+  },
+  {
+    name: "Sass",
+    icon: FaSass,
+  },
+  {
     name: "AWS",
     icon: FaAws,
+  },
+  {
+    name: "Cloudflare",
+    icon: FaCloudflare,
+  },
+  {
+    name: "Netlify",
+    icon: BiLogoNetlify,
   },
   {
     name: "Git",
@@ -39,20 +71,48 @@ const technologies = [
     name: "Figma",
     icon: FaFigma,
   },
+  {
+    name: "Framer",
+    icon: SiFramer,
+  },
+  {
+    name: "Sketch",
+    icon: FaSketch,
+  },
+  {
+    name: "Vite",
+    icon: TbBrandVite,
+  },
+  {
+    name: "NPM",
+    icon: FaNpm,
+  },
+  {
+    name: "Storybook",
+    icon: TbBrandStorybook,
+  },
+  {
+    name: "Mantine",
+    icon: SiMantine,
+  },
+  {
+    name: "Shadcn",
+    icon: SiShadcnui,
+  }
 ];
 
 const Technologies = () => {
   return (
     <section
-      className="p-4 justify-center w-7/12 text-center mb-8"
+      className="p-4 justify-center md:w-8/12 text-center mb-8"
       id="technologies"
     >
       <span className="text-3xl">Technologies</span>
-      <Box className="grid grid-cols-4 gap-4 justify-center align-middle mt-18 mb-18">
+      <Box className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center align-middle mt-18 mb-18">
         {technologies.map((tech) => (
           <motion.div
             key={tech.name}
-            className="flex flex-row p-4 border rounded-4xl border-gray-200 align-middle justify-center gap-4 w-40"
+            className="flex flex-row p-4 border rounded-4xl border-gray-200 align-middle justify-center gap-4 w-42"
             initial={{ y: 50, opacity: 0 }}
             whileInView={{ y: 0, opacity: 1 }}
             viewport={{ once: true }}
