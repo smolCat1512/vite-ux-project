1ced34e63a99224d5b9e087f96104c9e0d18fe3e
ShaunH1512 <shaunhalliday1512@gmail.com>
2026-04-18 08:08:21 +0100

    chore: Cleanup, remove commented JSX and tidy Hero component
    Clean up Hero.tsx by removing commented section labels and a commented-out HeroDescription, and reformatting the ContactButton children. This is a non-functional cleanup to reduce clutter and improve readability.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,28 +13,20 @@ const Hero = () => {
   return (
     <Box className="flex flex-col">
       <Box className="text-center justify-center mx-auto w-10/12 md:w-auto lg:w-auto">
-        {/* Main heading */}
         <div className="flex flex-row items-center justify-center md:pb-12">
           <HeroHeading animate={!shouldReduceMotion}>
             Hi, I'm <HeroHighlight>Shaun</HeroHighlight> I <RoleType />
           </HeroHeading>
         </div>
-
-        {/* Subheading */}
         <div className="pb-4">
           <HeroSubheading animate={!shouldReduceMotion}>
             Front-end Software Engineer and UX/UI Engineer
           </HeroSubheading>
         </div>
-
-        {/* Description
-        <HeroDescription animate={!shouldReduceMotion}>
-          I create user-centered digital experiences
-        </HeroDescription> */}
-
-        <ContactButton animate={!shouldReduceMotion}>Get In Touch</ContactButton>
+        <ContactButton animate={!shouldReduceMotion}>
+          Get In Touch
+        </ContactButton>
       </Box>
-
       <ScrollDownButton animate={!shouldReduceMotion} />
     </Box>
   );
